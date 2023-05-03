b597596df73d9e03fb2058ad19c1ecd5bf4e49f4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-03 21:36:57 +0900

    変更 - pokemonAPIへ接続し、urqlの素振りをしてみる GrapQLって専用サーバーじゃないとだめぽい（RESTAPIはたたけない？

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Client, cacheExchange, fetchExchange } from "urql"
+import { Client, Provider, cacheExchange, fetchExchange } from "urql"
 import App from "./App.tsx"
 import "./index.css"
 
 const client = new Client({
-  url: "",
+  url: "https://beta.pokeapi.co/graphql/v1beta",
   exchanges: [cacheExchange, fetchExchange],
 })
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
