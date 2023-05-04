19f87f1ddb805c18bc9eff7b64034784cd80a49c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-04 23:02:25 +0900

    変更 - urqlを外してActionのテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
-import { useState } from "react"
-import { useQuery, gql } from "urql"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-
-const getPokemons = gql(`
-  query getPokemons {
-    pokemon_v2_pokemon(limit:30, where:{pokemon_species_id: {_gt: 500}}) {
-      name
-      id
-      pokemon_v2_pokemonsprites {
-        sprites
-      }
-    }
-  }
-`)
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [result] = useQuery({ query: getPokemons })
-
-  console.log({ result })
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='card'>
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -41,11 +25,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { Client, Provider, cacheExchange, fetchExchange } from "urql"
-import App from "./App.tsx"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-const client = new Client({
-  url: "https://beta.pokeapi.co/graphql/v1beta",
-  exchanges: [cacheExchange, fetchExchange],
-})
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider value={client}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
-)
+);
